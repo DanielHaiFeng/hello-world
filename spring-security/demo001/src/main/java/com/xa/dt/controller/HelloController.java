9b6208b5e5970e07a837d3f7db063d8add55de9b
dangting <dangting@boco.com.cn>
2019-05-29 11:32:02 +0800

    提交spring-cloud和nacos集成代码

---
+++ spring-security/demo001/src/main/java/com/xa/dt/controller/HelloController.java
@@ -0,0 +1,35 @@
+package com.xa.dt.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * @author DangTing[dangting@boco.com.cn]
+ * @classname HelloController
+ * @date 2019-05-15 16:39
+ * @version: 1.0
+ * @description: TODO
+ */
+@Controller
+public class HelloController {
+
+    @RequestMapping(value = { "/", "/welcome**" }, method = RequestMethod.GET)
+    public ModelAndView welcomePage() {
+        ModelAndView model = new ModelAndView();
+        model.addObject("title", "Spring Security Hello World");
+        model.addObject("message", "This is welcome page!");
+        model.setViewName("hello");
+        return model;
+    }
+
+    @RequestMapping(value = "/admin**", method = RequestMethod.GET)
+    public ModelAndView adminPage() {
+        ModelAndView model = new ModelAndView();
+        model.addObject("title", "Spring Security Hello World");
+        model.addObject("message", "This is protected page!");
+        model.setViewName("admin");
+        return model;
+    }
+}
