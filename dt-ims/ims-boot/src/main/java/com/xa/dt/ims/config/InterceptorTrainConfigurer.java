41ac18875ca8ecab669897527be4599cef2d7eb3
dangting <dangting@boco.com.cn>
2020-01-08 15:12:36 +0800

    提交注解使用实例及测试用例

---
+++ dt-ims/ims-boot/src/main/java/com/xa/dt/ims/config/InterceptorTrainConfigurer.java
@@ -0,0 +1,15 @@
+package com.xa.dt.ims.config;
+
+import com.xa.dt.ims.interceptor.SourceAccessInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class InterceptorTrainConfigurer implements WebMvcConfigurer {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new SourceAccessInterceptor()).addPathPatterns("/**");
+    }
+}
